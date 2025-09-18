3559bcdfdb06dd33adb5677438f73f1165dc762a
amanda754 <amanda@pgcbasketball.com>
2025-09-18 09:04:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your smoothie will be:', name_on_order)
 
 # Get the Snowpark session
-cnx - st.connection("snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 
 # Query the fruit names
